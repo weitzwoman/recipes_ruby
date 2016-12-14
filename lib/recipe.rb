8af23301743db8bbc82a8e5fdb02b95d76f941f6
Jonathan Thom and Tracie Weitzman <jonathan@>
2016-12-14 11:44:20 -0800

    add validation presence for recipe form with passing specs

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:categories)
+  validates(:name, :presence => true)
+  validates(:ingredients, :presence => true)
+  validates(:instructions, :presence => true)
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Recipe) do
   it { should have_and_belong_to_many(:categories)}
+
+  it('validates presence of recipe name') do
+    test_recipe = Recipe.new({:name => "", :ingredients => "milk", :instructions => "drink it"})
+    expect(test_recipe.save()).to(eq(false))
+  end
 end
