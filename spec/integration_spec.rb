5460db7a65a9cfd00de279a08383cbad3005eae2
Jonathan Thom and Tracie Weitzman <tracie@>
2016-12-14 14:46:16 -0800

    pass add recipe integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -7,4 +7,13 @@ describe('the add category route', {:type => :feature}) do
     click_button('Add Category')
     expect(page).to have_content('JAPANESE')
   end
+
+  it('will add a new recipe') do
+    visit('/')
+    fill_in('name', :with => 'Sushi')
+    fill_in('ingredients', :with => 'Rice')
+    fill_in('instructions', :with => 'Roll it!')
+    click_button('Add Recipe')
+    expect(page).to have_content('Sushi')
+  end
 end
