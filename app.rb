5b36dc7885b0f49717a3a11f310a26ab7a066b62
Jonathan Thom and Tracie Weitzman <tracie@>
2016-12-14 09:15:20 -0800

    Add new category

--- app.rb
+++ app.rb
@@ -3,3 +3,23 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  #all categories
+  #all recipes
+  erb(:index)
+end
+
+post('/categories') do
+  @new_category = Category.new(:name => params['name'])
+  if @new_category.save()
+    redirect('/categories/'.concat(@new_category.id().to_s()))
+  else
+    erb(:index)
+  end
+end
+
+get('/categories/:id') do
+  @category = Category.find(params['id'].to_i())
+  erb(:category)
+end
