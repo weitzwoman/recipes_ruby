c357bfb91e36ef50cefeea435b151e764f229e40
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-15 09:32:39 -0800

    will refactor specs to use shoulda

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -3,16 +3,9 @@ require('spec_helper')
 describe(Category) do
   it { should have_and_belong_to_many(:recipes)}
 
-  it('validates presence of category name') do
-    test_category = Category.new({:name => ""})
-    expect(test_category.save()).to(eq(false))
-  end
-  it('validates uniqueness of category name') do
-    test_category = Category.new({:name => 'Hearty'})
-    test_category.save()
-    test_category1 = Category.new({:name => 'Hearty'})
-    expect(test_category1.save()).to(eq(false))
-  end
+  it { should validate_presence_of(:name) }
+
+  it { should validate_uniqueness_of(:name)}
 
   it('capitalizes a new category') do
     test_category = Category.create({:name => 'mexican'})
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,28 +3,13 @@ require('spec_helper')
 describe(Recipe) do
   it { should have_and_belong_to_many(:categories)}
 
-  it('validates presence of recipe name') do
-    test_recipe = Recipe.new({:name => "", :ingredients => "milk", :instructions => "drink it"})
-    expect(test_recipe.save()).to(eq(false))
-  end
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:instructions) }
 
-  it('validates uniqueness of recipe name') do
-    test_recipe = Recipe.new({:name => 'Mac and Cheese'})
-    test_recipe.save()
-    test_recipe1 = Recipe.new({:name => 'Mac and Cheese'})
-    expect(test_recipe1.save()).to(eq(false))
-  end
+  it { should validate_uniqueness_of(:name)}
 
   it('capitalizes all words of a recipe name') do
     test_recipe = Recipe.create({:name => 'fish tacos', :ingredients => "fish", :instructions => "eat it"})
     expect(test_recipe.name()).to(eq('Fish Tacos'))
   end
-
-  # describe('.rating_sort') do
-  #   it('sorts the recipes by rating descending') do
-  #     test_recipe = Recipe.create({:name => 'Bad Cookies', :rating => 1})
-  #     test_recipe1 = Recipe.create({:name => 'Cheesy Potatoes', :rating => 2})
-  #     expect(Recipe.rating_sort()).to(eq([test_recipe1, test_recipe]))
-  #   end
-  # end
 end
